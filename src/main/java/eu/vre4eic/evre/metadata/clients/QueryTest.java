78e0bd4bec94f7f717cae9fcd71f210c94fee2c3
rousakis <rousakis@costner.ics.forth.gr>
2017-09-28 13:36:22 +0300

    Changes - Added license

--- src/main/java/eu/vre4eic/evre/metadata/clients/QueryTest.java
+++ src/main/java/eu/vre4eic/evre/metadata/clients/QueryTest.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2017 rousakis.
+ * Copyright 2017 VRE4EIC Consortium
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
