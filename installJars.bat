0eaaf39924e3a7fcb7da4363b6d03c95a2afd831
rousakis <rousakis@costner.ics.forth.gr>
2018-09-27 17:14:05 +0300

    Changes - added required maven jars for the connection with Virtuoso and Blazegraph

--- installJars.bat
+++ installJars.bat
@@ -1,6 +1,7 @@
 call mvn install:install-file -Dfile="libs\NodeService-0.0.1-SNAPSHOT-classes.jar" -DartifactId=NodeService -DgroupId=eu.vre4eic.evre -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
-call mvn install:install-file -Dfile="libs\BlazegraphOps-1.0-SNAPSHOT.jar" -DartifactId=BlazegraphOps -DgroupId=eu.vre4eic.evre -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
 call mvn install:install-file -Dfile="libs\NodeService-0.0.3-SNAPSHOT-classes.jar" -DartifactId=NodeService -DgroupId=eu.vre4eic.evre -Dversion=0.0.3-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
+call mvn install:install-file -Dfile="libs\BlazegraphOps-1.0-SNAPSHOT.jar" -DartifactId=BlazegraphOps -DgroupId=forth.ics -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
+call mvn install:install-file -Dfile="libs\VirtuosoOpsImpl-1.0-SNAPSHOT.jar" -DartifactId=VirtuosoOpsImpl -DgroupId=forth.ics -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
 pause
 
 
